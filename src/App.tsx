96d27a835fa1c9375146cbfd2288c214f740ec06
joao-gulin <joaogulin@gmail.com>
2025-02-27 21:46:33 +0000

    sidebar wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import SidebarLayout from './components/Wrappers/SideBarLayout'
 import Main from './pages/Main'
 
 const queryClient = new QueryClient()
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-        <Main />
+        <SidebarLayout>
+          <Main />
+        </SidebarLayout>
         <ReactQueryDevtools />
     </QueryClientProvider>
   )
