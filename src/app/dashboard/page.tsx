3f303b22b327ed8c5f68b40ec092228de4ede3a2
joao-gulin <joaogulin@gmail.com>
2025-02-27 21:31:30 +0000

    sidebar added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,3 @@
-import ClientsTable from "@/components/ClientsTable";
-import { useClient } from "../hooks/useClients";
-import ClientSkeleton from "@/components/ClientSkeleton";
 import { AppSidebar } from "@/components/app-sidebar"
 import {
   Breadcrumb,
@@ -17,34 +14,18 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 
-export default function Main() {
-  const { isLoading, isError, clients } = useClient()
-
-  if (isLoading) {
-    return (
-      <ClientSkeleton />
-    )
-  }
-
-  if (isError) {
-    return (
-      <div>Error...</div>
-    )
-  }
-
-  if (!clients) {
-    return <div>No clients available.</div>;
-  }
-
+export default function Page() {
   return (
-    <>
-      <SidebarProvider>
+    <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
@@ -61,14 +42,14 @@ export default function Main() {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="p-6 max-w-4xl mx-auto space-y-4">
-          <ClientsTable 
-            data={clients}
-          />
-        </div>
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+            <div className="bg-muted/50 aspect-video rounded-xl" />
+          </div>
+          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
         </div>
       </SidebarInset>
     </SidebarProvider>
-    </>
   )
 }
